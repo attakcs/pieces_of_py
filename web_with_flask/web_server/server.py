df9f8f8346932522dd57504a8b646403dd588645
brthzz <brthzz@gmail.com>
2021-02-28 13:14:22 +0100

    WebDev w/ Flask, Contact From [Request, Redirect]

--- web_with_flask/web_server/server.py
+++ web_with_flask/web_server/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,6 +8,16 @@ def home():
 @app.route('/<string:page_name>')
 def html_page(page_name):
     return render_template(page_name + '.html')
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        print(data)
+        return redirect('/thankyou')
+    else:
+        return 'something went wrong'
+
 
 if __name__ == '__main__':
     app.run(debug=True)
