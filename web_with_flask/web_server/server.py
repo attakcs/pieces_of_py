8e9ec2a952dd7ce46a7a455062569266f0fff3c8
brthzz <brthzz@gmail.com>
2021-02-28 13:49:04 +0100

    WebDev w/ Flask, Contact From [How to write CSV file]

--- web_with_flask/web_server/server.py
+++ web_with_flask/web_server/server.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, url_for, request, redirect
+import csv
 app = Flask(__name__)
 
 @app.route('/')
@@ -9,11 +10,20 @@ def home():
 def html_page(page_name):
     return render_template(page_name + '.html')
 
+# csv = comma separated values
+def write_to_csv(data):
+    with open('database.csv', mode='a') as database:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        csv_writer = csv.writer(database, delimiter=",", quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email,subject,message])
+
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
     if request.method == 'POST':
         data = request.form.to_dict()
-        print(data)
+        write_to_csv(data)
         return redirect('/thankyou')
     else:
         return 'something went wrong'
